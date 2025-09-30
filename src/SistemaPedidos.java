6d5183bf5f94c387454fe4e3773a953c0b1152a1
Vini <viniciusdelima2017@gmail.com>
2025-09-29 22:30:15 -0300

    Versão final

--- src/SistemaPedidos.java
+++ src/SistemaPedidos.java
@@ -2,24 +2,24 @@ public class SistemaPedidos {
     public static void main(String[] args) {
         ClientePJ cliente = new ClientePJ("Tech Solutions Ltda", "contato@tech.com", "12.345.678/0001-99");
 
-        Pedido pedidoCartaoAprovado = new Pedido(cliente);
-        pedidoCartaoAprovado.adicionarItem(new ItemPedido(new ProdutoDigital("Produto A", 150.0), 1));
-        pedidoCartaoAprovado.setMetodoPagamento(new PagamentoCartaoCredito());
-        pedidoCartaoAprovado.processarPagamento();
+        Pedido pedido1 = new Pedido(cliente);
+        pedido1.adicionarItem(new ItemPedido(new ProdutoDigital("Produto A", 150.0), 1));
+        pedido1.setMetodoPagamento(new PagamentoCartaoCredito());
+        pedido1.processarPagamento();
 
-        Pedido pedidoCartaoRecusado = new Pedido(cliente);
-        pedidoCartaoRecusado.adicionarItem(new ItemPedido(new ProdutoDigital("Produto B", 500.0), 1));
-        pedidoCartaoRecusado.setMetodoPagamento(new PagamentoCartaoCredito());
-        pedidoCartaoRecusado.processarPagamento();
+        Pedido pedido2 = new Pedido(cliente);
+        pedido2.adicionarItem(new ItemPedido(new ProdutoDigital("Produto B", 500.0), 1));
+        pedido2.setMetodoPagamento(new PagamentoCartaoCredito());
+        pedido2.processarPagamento();
 
-        Pedido pedidoBoletoAprovado = new Pedido(cliente);
-        pedidoBoletoAprovado.adicionarItem(new ItemPedido(new ProdutoDigital("Produto C", 800.0), 1));
-        pedidoBoletoAprovado.setMetodoPagamento(new PagamentoBoleto());
-        pedidoBoletoAprovado.processarPagamento();
+        Pedido pedido3 = new Pedido(cliente);
+        pedido3.adicionarItem(new ItemPedido(new ProdutoDigital("Produto C", 800.0), 1));
+        pedido3.setMetodoPagamento(new PagamentoBoleto());
+        pedido3.processarPagamento();
 
-        Pedido pedidoBoletoRecusado = new Pedido(cliente);
-        pedidoBoletoRecusado.adicionarItem(new ItemPedido(new ProdutoDigital("Produto D", 1500.0), 1));
-        pedidoBoletoRecusado.setMetodoPagamento(new PagamentoBoleto());
-        pedidoBoletoRecusado.processarPagamento();
+        Pedido pedido4 = new Pedido(cliente);
+        pedido4.adicionarItem(new ItemPedido(new ProdutoDigital("Produto D", 1500.0), 1));
+        pedido4.setMetodoPagamento(new PagamentoBoleto());
+        pedido4.processarPagamento();
     }
 }
